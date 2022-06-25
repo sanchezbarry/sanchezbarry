b61165e881c1e1fcc5b639a842c03e497a497744
sanchezbarry <sanchezbarry@gmail.com>
2022-06-25 14:25:22 +0800

    some css, sharing feature for FB WA & TELE

--- app.js
+++ app.js
@@ -215,6 +215,34 @@ async function randomDrink(randomUrl){
 }
 
 
+//social share
+const viewBtn = document.querySelector("#share"),
+popup = document.querySelector(".popup"),
+close = popup.querySelector(".close"),
+field = popup.querySelector(".field"),
+input = field.querySelector("input"),
+copy = field.querySelector("button");
+
+viewBtn.onclick = ()=>{
+  popup.classList.toggle("show");
+}
+close.onclick = ()=>{
+  viewBtn.click();
+}
+
+copy.onclick = ()=>{
+  input.select(); //select input value
+  if(document.execCommand("copy")){ //if the selected text copy
+    field.classList.add("active");
+    copy.innerText = "Copied";
+    setTimeout(()=>{
+      window.getSelection().removeAllRanges(); //remove selection from document
+      field.classList.remove("active");
+      copy.innerText = "Copy";
+    }, 3000);
+  }
+}
+
 
 // const learnToMake = document.querySelector('#results')
 // learnToMake.onclick = function (event) {
