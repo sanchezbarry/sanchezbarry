60295eea9bfa42e55bb3108aae43967625034e73
sanchezbarry <sanchezbarry@gmail.com>
2022-06-30 20:08:40 +0800

    fav drink feature, clean up code

--- app.js
+++ app.js
@@ -1,136 +1,151 @@
-
 //this combines the user's alcohol choice with the partial api url to pull data of the chosen alcohol
 const alcoholChoice = document.querySelector('#alcohol')
 alcoholChoice.addEventListener('click', event => {
-    const chosenAlcohol = event.target.innerText
-    console.log(chosenAlcohol)
+  const chosenAlcohol = event.target.innerText
+  console.log(chosenAlcohol)
 
-    const urlToFetch = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + chosenAlcohol
-    console.log(urlToFetch)
+  const urlToFetch = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + chosenAlcohol
+  console.log(urlToFetch)
 
-    fetchTheDrinks(urlToFetch)
-
+  fetchTheDrinks(urlToFetch)
 })
 
 
 // churns the cocktail results of the chosen alcohol. This also assigns the cocktail ID to the id of the button.
 async function fetchTheDrinks(urlToFetch) {
-    const response = await fetch(urlToFetch)
-    console.log('response: ', response)
-    const data = await response.json()
+  const response = await fetch(urlToFetch)
+  console.log('response: ', response)
+  const data = await response.json()
 
-    const cocktailDivList = document.getElementById('results')
-    cocktailDivList.innerHTML = ''
+  const cocktailDivList = document.getElementById('results')
+  cocktailDivList.innerHTML = ''
 
-    const cocktailRecipeList = document.getElementById('cocktails')
-    cocktailRecipeList.innerHTML = ''
+  const cocktailRecipeList = document.getElementById('cocktails')
+  cocktailRecipeList.innerHTML = ''
 
-    const cocktailRandomList = document.getElementById('random-drink')
-    cocktailRandomList.innerHTML = ''
+  const cocktailRandomList = document.getElementById('random-drink')
+  cocktailRandomList.innerHTML = ''
 
-    for (let x = 0; x < data.drinks.length; x++) {
+  for (let x = 0; x < data.drinks.length; x++) {
 
-        const cocktail = data.drinks[x]
-        const cocktailDiv = document.createElement('div')
-        cocktailDiv.setAttribute('class', 'col shadow-sm p-3 mb-5 rounded text-center')
-        cocktailDiv.setAttribute('id', 'cocktail')
-        cocktailDivList.append(cocktailDiv)
+    const cocktail = data.drinks[x]
+    const cocktailDiv = document.createElement('div')
+    cocktailDiv.setAttribute('class', 'col shadow-sm p-3 mb-5 rounded text-center')
+    cocktailDiv.setAttribute('id', 'cocktail')
+    cocktailDivList.append(cocktailDiv)
 
-        const cocktailImg = document.createElement('img');
-        cocktailImg.src = cocktail.strDrinkThumb;
-        cocktailImg.setAttribute('id', 'cocktail-img')
-        cocktailImg.setAttribute('class', 'rounded mx-auto d-block')
-        cocktailDiv.appendChild(cocktailImg);
+    const cocktailImg = document.createElement('img');
+    cocktailImg.src = cocktail.strDrinkThumb;
+    cocktailImg.setAttribute('id', 'cocktail-img')
+    cocktailImg.setAttribute('class', 'rounded mx-auto d-block')
+    cocktailDiv.appendChild(cocktailImg);
 
-        const cocktailName = cocktail.strDrink
-        const heading = document.createElement('h4')
-        heading.innerHTML = cocktailName
-        cocktailDiv.appendChild(heading)
+    const cocktailName = cocktail.strDrink
+    const heading = document.createElement('h4')
+    heading.innerHTML = cocktailName
+    cocktailDiv.appendChild(heading)
 
-        const ingredientBtn = document.createElement('button')
-        ingredientBtn.setAttribute('type', 'button')
-        ingredientBtn.setAttribute('class', 'btn btn-secondary')
+    const ingredientBtn = document.createElement('button')
+    ingredientBtn.setAttribute('type', 'button')
+    ingredientBtn.setAttribute('class', 'btn btn-secondary')
 
-        ingredientBtn.innerText = "Learn to Make"
-        const drinkId = cocktail.idDrink
-        ingredientBtn.setAttribute('id', `${drinkId}`)
-        cocktailDiv.appendChild(ingredientBtn)
+    ingredientBtn.innerText = "Learn to Make"
+    const drinkId = cocktail.idDrink
+    ingredientBtn.setAttribute('id', `${drinkId}`)
+    cocktailDiv.appendChild(ingredientBtn)
 
-        document.getElementById(drinkId).addEventListener('click', learnToMake)
-
-
-
+    document.getElementById(drinkId).addEventListener('click', learnToMake)
     }
-
 }
 
 //clears the previous result and combines the id of the button to make the api call for cocktail details
 const learnToMake = () => {
-    console.log('i will work. testing')
-    const resultsElement = document.querySelector('#cocktails')
-    resultsElement.innerHTML = ''
+  console.log('i will work. testing')
+  const resultsElement = document.querySelector('#cocktails')
+  resultsElement.innerHTML = ''
 
-    const cocktailRandomList = document.getElementById('random-drink')
-    cocktailRandomList.innerHTML = ''
+  const cocktailRandomList = document.getElementById('random-drink')
+  cocktailRandomList.innerHTML = ''
 
-    const cocktailDivList = document.getElementById('results')
-    cocktailDivList.innerHTML = ''
+  const cocktailDivList = document.getElementById('results')
+  cocktailDivList.innerHTML = ''
 
-    const drinkId2 = event.target.id
-    console.log(drinkId2)
+  const cocktailSavedDrinks = document.getElementById('fav-container')
+  cocktailSavedDrinks.innerHTML = ''
 
-    const recipeToFetch = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + drinkId2
-    console.log(recipeToFetch)
+  const drinkId2 = event.target.id
+  console.log(drinkId2)
 
-    recipeFetched(recipeToFetch)
+  const recipeToFetch = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + drinkId2
+  console.log(recipeToFetch)
 
+  recipeFetched(recipeToFetch)
 }
 
+//to show saved drinks
+const saveDrink = (e) => { 
+  const cocktailName = e.target.dataset.drinkName
+  console.log(cocktailName)
+
+  const cocktailImg = e.target.dataset.drinkImg
+  console.log(cocktailImg)
+
+  localStorage.setItem('drinkName', cocktailName)
+  localStorage.setItem('drinkImg', cocktailImg)
+
+  console.log(localStorage.getItem('drinkName'))
+  console.log(localStorage.getItem('drinkImg'))
+}
+
+
+//clears and fetches drink recipe, creates recipe name, img, instructions, ingredients.
 async function recipeFetched(recipeToFetch){
-    const response = await fetch(recipeToFetch)
-    console.log('response: ', response)
-    const data = await response.json()
+  const response = await fetch(recipeToFetch)
+  console.log('response: ', response)
+  const data = await response.json()
 
+  const cocktailRecipeList = document.getElementById('cocktails')
+  cocktailRecipeList.innerHTML = ''
 
-    const cocktailRecipeList = document.getElementById('cocktails')
-    cocktailRecipeList.innerHTML = ''
+  const cocktailDivList = document.getElementById('results')
+  cocktailDivList.innerHTML = ''
 
-    const cocktailDivList = document.getElementById('results')
-    cocktailDivList.innerHTML = ''
+  const cocktailRandomList = document.getElementById('random-drink')
+  cocktailRandomList.innerHTML = ''
 
-    const cocktailRandomList = document.getElementById('random-drink')
-    cocktailRandomList.innerHTML = ''
+  const cocktailSavedDrinks = document.getElementById('fav-container')
+  cocktailSavedDrinks.innerHTML = ''
 
-    const cocktail = data.drinks[0]
-    const cocktailDiv = document.createElement('div')
-    cocktailDiv.setAttribute('class', 'col shadow-sm p-3 mb-5 rounded text-center')
-    cocktailDiv.setAttribute('id', 'cocktail')
-    cocktailRecipeList.append(cocktailDiv)
+  const cocktail = data.drinks[0]
+  const cocktailDiv = document.createElement('div')
+  cocktailDiv.setAttribute('class', 'col shadow-sm p-3 mb-5 rounded text-center')
+  cocktailDiv.setAttribute('id', 'cocktail')
+  cocktailRecipeList.append(cocktailDiv)
 
-    const cocktailImg = document.createElement('img');
-    cocktailImg.src = cocktail.strDrinkThumb;
-    cocktailImg.setAttribute('id', 'cocktail-img')
-    cocktailImg.setAttribute('class', 'rounded mx-auto d-block img-fluid')
-    cocktailDiv.appendChild(cocktailImg);
+  const cocktailImg = document.createElement('img');
+  cocktailImg.src = cocktail.strDrinkThumb;
+  cocktailImg.setAttribute('id', 'cocktail-img')
+  cocktailImg.setAttribute('class', 'rounded mx-auto d-block img-fluid')
+  cocktailDiv.appendChild(cocktailImg);
 
-    const cocktailName = cocktail.strDrink
-    const heading = document.createElement('h4')
-    heading.innerHTML = cocktailName
-    cocktailDiv.appendChild(heading)
+  const cocktailName = cocktail.strDrink
+  const heading = document.createElement('h4')
+  heading.innerHTML = cocktailName
+  cocktailDiv.appendChild(heading)
     
-    const cocktailIngredients = document.createElement("ul");
-    cocktailDiv.appendChild(cocktailIngredients);  
+  const cocktailIngredients = document.createElement("ul");
+  cocktailDiv.appendChild(cocktailIngredients);  
     
-    const getIngredients = Object.keys(cocktail)
-      .filter(function (ingredient) {
-        return ingredient.indexOf("strIngredient") == 0;
-      })
-      .reduce(function (ingredients, ingredient) {
-        if (cocktail[ingredient] != null) {
-          ingredients[ingredient] = cocktail[ingredient];
-        }
-        return ingredients;
-      }, {});
+  const getIngredients = Object.keys(cocktail)
+    .filter(function (ingredient) {
+      return ingredient.indexOf("strIngredient") == 0;
+    })
+    .reduce(function (ingredients, ingredient) {
+      if (cocktail[ingredient] != null) {
+        ingredients[ingredient] = cocktail[ingredient];
+      }
+      return ingredients;
+    }, {});
   
     for (let key in getIngredients) {
       let value = getIngredients[key];
@@ -139,67 +154,118 @@ async function recipeFetched(recipeToFetch){
       cocktailIngredients.appendChild(listItem);
     }
 
-    const instructions = cocktail.strInstructions
-    const bodyText = document.createElement('p')
-    bodyText.innerHTML = instructions
-    cocktailDiv.appendChild(bodyText)
+  const instructions = cocktail.strInstructions
+  const bodyText = document.createElement('p')
+  bodyText.innerHTML = instructions
+  cocktailDiv.appendChild(bodyText)
+
+  const saveBtn = document.createElement('button')
+  saveBtn.setAttribute('type', 'button')
+  saveBtn.setAttribute('class', 'btn btn-secondary')
+  saveBtn.dataset.drinkName = cocktailName
+  saveBtn.dataset.drinkImg = cocktailImg.src
+
+  saveBtn.innerText = "Save Drink"
+  const drinkId = cocktail.idDrink
+  saveBtn.setAttribute('id', `${drinkId}`)
+  cocktailDiv.appendChild(saveBtn)
+
+  document.getElementById(drinkId).addEventListener('click', saveDrink)
+}
+
+// function to show fav drink
+const showFavDrink = () => {
+  const cocktailRecipeList = document.getElementById('cocktails')
+  cocktailRecipeList.innerHTML = ''
+
+  const cocktailDivList = document.getElementById('results')
+  cocktailDivList.innerHTML = ''
+
+  const cocktailRandomList = document.getElementById('random-drink')
+  cocktailRandomList.innerHTML = ''
+
+  const cocktailSavedDrinks = document.getElementById('fav-container')
+  cocktailSavedDrinks.innerHTML = ''
+
+  const cocktailDiv = document.createElement('div')
+  cocktailDiv.setAttribute('class', 'col shadow-sm p-3 mb-5 rounded text-center')
+  cocktailDiv.setAttribute('id', 'cocktail')
+  cocktailSavedDrinks.appendChild(cocktailDiv)
+
+  const cocktailImg = document.createElement('img');
+  cocktailImg.src = localStorage.getItem('drinkImg')
+  cocktailImg.setAttribute('id', 'cocktail-img')
+  cocktailImg.setAttribute('class', 'rounded mx-auto d-block img-fluid')
+  cocktailDiv.appendChild(cocktailImg);
+
+  const cocktailName = localStorage.getItem('drinkName')
+  const heading = document.createElement('h4')
+  heading.innerHTML = cocktailName
+  cocktailDiv.appendChild(heading)
 
 }
 
+//event listener for favdrink
+document.getElementById('fav-drink').addEventListener('click', showFavDrink)
+
+//random drink btn
 const randomDrinkBtn = document.querySelector('#random-button')
 randomDrinkBtn.addEventListener('click', event => {
-    randomDrink(randomUrl)
-        }
-    )
+  randomDrink(randomUrl)
+      }
+  )
 
-
+//random drink API url
 const randomUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
 
+//random drink api call, creates recipe name, img, instructions, ingredients.
 async function randomDrink(randomUrl){
-    const response = await fetch(randomUrl)
-    console.log('response: ', response)
-    const data = await response.json()
+  const response = await fetch(randomUrl)
+  console.log('response: ', response)
+  const data = await response.json()
 
-    const cocktailRecipeList = document.getElementById('cocktails')
-    cocktailRecipeList.innerHTML = ''
+  const cocktailRecipeList = document.getElementById('cocktails')
+  cocktailRecipeList.innerHTML = ''
 
-    const cocktailDivList = document.getElementById('results')
-    cocktailDivList.innerHTML = ''
+  const cocktailDivList = document.getElementById('results')
+  cocktailDivList.innerHTML = ''
 
-    const cocktailRandomList = document.getElementById('random-drink')
-    cocktailRandomList.innerHTML = ''
+  const cocktailRandomList = document.getElementById('random-drink')
+  cocktailRandomList.innerHTML = ''
 
+  const cocktailSavedDrinks = document.getElementById('fav-container')
+  cocktailSavedDrinks.innerHTML = ''
 
-    const cocktail = data.drinks[0]
-    const cocktailDiv = document.createElement('div')
-    cocktailDiv.setAttribute('class', 'col shadow-sm p-3 mb-5 rounded text-center')
-    cocktailDiv.setAttribute('id', 'cocktail')
-    cocktailRandomList.append(cocktailDiv)
+  const cocktail = data.drinks[0]
+  const cocktailDiv = document.createElement('div')
+  cocktailDiv.setAttribute('class', 'col shadow-sm p-3 mb-5 rounded text-center')
+  cocktailDiv.setAttribute('id', 'cocktail')
+  cocktailRandomList.append(cocktailDiv)
 
-    const cocktailImg = document.createElement('img');
-    cocktailImg.src = cocktail.strDrinkThumb;
-    cocktailImg.setAttribute('id', 'cocktail-img')
-    cocktailImg.setAttribute('class', 'rounded mx-auto d-block img-fluid')
-    cocktailDiv.appendChild(cocktailImg);
+  const cocktailImg = document.createElement('img');
+  cocktailImg.src = cocktail.strDrinkThumb;
+  cocktailImg.setAttribute('id', 'cocktail-img')
+  cocktailImg.setAttribute('class', 'rounded mx-auto d-block img-fluid')
+  cocktailDiv.appendChild(cocktailImg);
 
-    const cocktailName = cocktail.strDrink
-    const heading = document.createElement('h4')
-    heading.innerHTML = cocktailName
-    cocktailDiv.appendChild(heading)
+  const cocktailName = cocktail.strDrink
+  const heading = document.createElement('h4')
+  heading.innerHTML = cocktailName
+  cocktailDiv.appendChild(heading)
     
-    const cocktailIngredients = document.createElement("ul");
-    cocktailDiv.appendChild(cocktailIngredients);  
+  const cocktailIngredients = document.createElement("ul");
+  cocktailDiv.appendChild(cocktailIngredients);  
     
-    const getIngredients = Object.keys(cocktail)
-      .filter(function (ingredient) {
-        return ingredient.indexOf("strIngredient") == 0;
-      })
-      .reduce(function (ingredients, ingredient) {
-        if (cocktail[ingredient] != null) {
-          ingredients[ingredient] = cocktail[ingredient];
-        }
-        return ingredients;
-      }, {});
+  const getIngredients = Object.keys(cocktail)
+    .filter(function (ingredient) {
+      return ingredient.indexOf("strIngredient") == 0;
+    })
+    .reduce(function (ingredients, ingredient) {
+      if (cocktail[ingredient] != null) {
+        ingredients[ingredient] = cocktail[ingredient];
+      }
+      return ingredients;
+    }, {});
   
     for (let key in getIngredients) {
       let value = getIngredients[key];
@@ -208,12 +274,24 @@ async function randomDrink(randomUrl){
       cocktailIngredients.appendChild(listItem);
     }
 
-    const instructions = cocktail.strInstructions
-    const bodyText = document.createElement('p')
-    bodyText.innerHTML = instructions
-    cocktailDiv.appendChild(bodyText)
-}
+  const instructions = cocktail.strInstructions
+  const bodyText = document.createElement('p')
+  bodyText.innerHTML = instructions
+  cocktailDiv.appendChild(bodyText)
 
+  const saveBtn = document.createElement('button')
+  saveBtn.setAttribute('type', 'button')
+  saveBtn.setAttribute('class', 'btn btn-secondary')
+  saveBtn.dataset.drinkName = cocktailName
+  saveBtn.dataset.drinkImg = cocktailImg.src
+
+  saveBtn.innerText = "Save Drink"
+  const drinkId = cocktail.idDrink
+  saveBtn.setAttribute('id', `${drinkId}`)
+  cocktailDiv.appendChild(saveBtn)
+
+  document.getElementById(drinkId).addEventListener('click', saveDrink)    
+}
 
 //social share
 const viewBtn = document.querySelector("#share"),
@@ -243,52 +321,3 @@ copy.onclick = ()=>{
   }
 }
 
-
-// const learnToMake = document.querySelector('#results')
-// learnToMake.onclick = function (event) {
-//     if (event.target.tagName === 'BUTTON') {
-//         console.log('does this work')
-//         const recipeStart = event.target.parentElement.parentElement
-//         recipeStart.innerHTML = ''
-
-//         
-
-//         console.log(recipeToFetch)
-//         // fetchTheRecipe(recipeToFetch)
-
-//     }
-// }
-
-
-// async function fetchTheRecipe(recipeToFetch){
-//     const response = await fetch(recipeToFetch)
-//     console.log('response: ',response)
-//     const data = await response.json()
-
-//     const recipeToPrint = data.drinks[0]
-
-//     console.log()
-// }
-
-
-// const recipeContainer = document.getElementById('recipe')
-// const cocktailIngredients = document.createElement("ul");
-// recipeContainer.appendChild(cocktailIngredients);  
-
-// const getIngredients = Object.keys(cocktail)
-//   .filter(function (ingredient) {
-//     return ingredient.indexOf("strIngredient") == 0;
-//   })
-//   .reduce(function (ingredients, ingredient) {
-//     if (cocktail[ingredient] != null) {
-//       ingredients[ingredient] = cocktail[ingredient];
-//     }
-//     return ingredients;
-//   }, {});
-
-// for (let key in getIngredients) {
-//   let value = getIngredients[key];
-//   listItem = document.createElement("li");
-//   listItem.innerHTML = value;
-//   cocktailIngredients.appendChild(listItem);
-// }
